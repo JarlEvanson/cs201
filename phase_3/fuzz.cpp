921fa93299c8ef59a7138f822811e407ad3733b4
Jarl Evanson <evanson.jarl@gmail.com>
2024-04-18 22:27:55 -0500

    Implemented better fuzzing harness for BHeap

--- phase_3/fuzz.cpp
+++ phase_3/fuzz.cpp
@@ -9,6 +9,19 @@
 #include <stdlib.h>
 #include <cassert>
 
+#include <vector>
+#include <queue>
+
+#ifndef __AFL_FUZZ_TESTCASE_LEN
+  ssize_t fuzz_len;
+  #define __AFL_FUZZ_TESTCASE_LEN fuzz_len
+  unsigned char fuzz_buf[1024000];
+  #define __AFL_FUZZ_TESTCASE_BUF fuzz_buf
+  #define __AFL_FUZZ_INIT() void sync(void);
+  #define __AFL_LOOP(x) ((fuzz_len = read(0, fuzz_buf, sizeof(fuzz_buf))) > 0 ? 1 : 0)
+  #define __AFL_INIT() sync()
+#endif
+
 __AFL_FUZZ_INIT();
 
 int main() {
@@ -18,28 +31,101 @@ int main() {
 
     while(__AFL_LOOP(UINT_MAX)) {
         unsigned int len = __AFL_FUZZ_TESTCASE_LEN;
+        unsigned int byte_index = 0;
         
-        BHeap<int> heap;
+        std::vector<BHeap<uint32_t>> heaps = std::vector<BHeap<uint32_t>>();
+        std::vector comparison_heaps = std::vector<std::priority_queue<uint32_t, std::vector<uint32_t>, std::greater<uint32_t>>>();
 
-        if(len < sizeof(int)) continue;
+        auto comparison = std::priority_queue<uint32_t, std::vector<uint32_t>, std::greater<uint32_t>>();
+        comparison_heaps.push_back(comparison);
 
-        int lowest = INT_MAX;
+        BHeap<uint32_t> heap;
+        heaps.push_back(heap);
 
-        for(int i = 0; i < len / sizeof(int); i++) {
-            int value = ((int) buf[i * sizeof(int)]) | (((int) buf[i * sizeof(int) + 1]) << 8) | (((int) buf[i * sizeof(int) + 2]) << 16) | (((int) buf[i * sizeof(int) + 3]) << 24);
-            if(value < lowest) {
-                lowest = value;
+        while(byte_index < len) {
+            if(byte_index + 5 < byte_index || byte_index + 5 > len) {
+                goto next_fuzz;
             }
-            heap.insert(value);
-            assert(heap.peekKey() == lowest);
+
+            unsigned char item_kind = buf[byte_index];
+            byte_index += 1;
+
+            uint32_t heap_index = (((uint32_t) buf[byte_index * sizeof(uint32_t)])
+                | (((uint32_t) buf[byte_index * sizeof(uint32_t) + 1]) << 8)
+                | (((uint32_t) buf[byte_index * sizeof(uint32_t) + 2]) << 16)
+                | (((uint32_t) buf[byte_index * sizeof(uint32_t) + 3]) << 24)) % heaps.size();
+            
+            byte_index += 4;
+
+            uint32_t value = 0;
+            uint32_t other_heap = 0;
+
+            switch(item_kind) {
+                case 0:
+                    if(byte_index + 4 < len || byte_index + 4 > len) {
+                        goto next_fuzz;
+                    }
+
+                    value = ((uint32_t) buf[byte_index * sizeof(uint32_t)])
+                        | (((uint32_t) buf[byte_index * sizeof(uint32_t) + 1]) << 8)
+                        | (((uint32_t) buf[byte_index * sizeof(uint32_t) + 2]) << 16)
+                        | (((uint32_t) buf[byte_index * sizeof(uint32_t) + 3]) << 24);
+
+                    byte_index += 4;
+
+                    heaps[heap_index].insert(value);
+                    comparison_heaps[heap_index].push(value);
+
+                    assert(heaps[heap_index].peekKey() == comparison_heaps[heap_index].top());
+
+                    break;
+                case 1:
+                    if(heaps[heap_index].isEmpty()) {
+                        continue;
+                    }
+
+                    assert(heaps[heap_index].extractMin() == comparison_heaps[heap_index].top());
+                    comparison_heaps[heap_index].pop();
+
+                    break;
+                case 2:
+                    if(byte_index + 4 < len || byte_index + 4 > len) {
+                        goto next_fuzz;
+                    }
+
+                    other_heap = (((uint32_t) buf[byte_index * sizeof(uint32_t)])
+                        | (((uint32_t) buf[byte_index * sizeof(uint32_t) + 1]) << 8)
+                        | (((uint32_t) buf[byte_index * sizeof(uint32_t) + 2]) << 16)
+                        | (((uint32_t) buf[byte_index * sizeof(uint32_t) + 3]) << 24)) % heaps.size();
+
+                    byte_index += 4;
+
+                    heaps[heap_index].merge(heaps[other_heap]);
+                    {
+                        while(!comparison_heaps[other_heap].empty()) {
+                            comparison_heaps[heap_index].push(comparison_heaps[other_heap].top());
+                            comparison_heaps[other_heap].pop();
+                        }
+                    }
+
+                    if(heaps[heap_index].isEmpty()) {
+                        continue;
+                    }
+                    assert(heaps[heap_index].peekKey() == comparison_heaps[heap_index].top());
+
+                    break;
+                case 3:
+                    heaps.push_back(heaps[heap_index]);
+                    comparison_heaps.push_back(comparison_heaps[heap_index]);
+
+                    break;
+                default:
+                    continue;
+            }
         }
 
-        while(!heap.isEmpty()) {
-            int extracted_value = heap.extractMin();
-
-            assert(extracted_value >= lowest);
-            lowest = extracted_value;
-        }
+    next_fuzz:
+        ;
     }
 
     return 0;

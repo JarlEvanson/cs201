677a7d12a3793a457b0b49467fcc0df7ccc973a4
Jarl Evanson <evanson.jarl@gmail.com>
2024-04-19 22:43:17 -0500

    Added test for merging

--- phase_3/test.cpp
+++ phase_3/test.cpp
@@ -14,6 +14,8 @@ void floats_test() {
     floats.insert(-10.0f);
     floats.insert(-20.0f);
 
+    assert(floats.element_count() == 6);
+
     assert(floats.extractMin() == -20.0);
     assert(floats.extractMin() == -10.0);
     assert(floats.extractMin() == 0.0);
@@ -22,7 +24,32 @@ void floats_test() {
     assert(floats.extractMin() == 10.0);
 }
 
+void merge_test() {
+    BHeap<int> heap1, heap2;
+
+    heap1.insert(0);
+
+    heap1.merge(heap2);
+
+    assert(heap1.element_count() == 1);
+    assert(heap1.extractMin() == 0);
+    assert(heap1.element_count() == 0);
+    assert(heap1.isEmpty() && heap2.isEmpty());
+
+    heap2.insert(2);
+
+    heap1.merge(heap2);
+
+    assert(heap1.element_count() == 1);
+    assert(heap1.extractMin() == 2);
+    assert(heap1.element_count() == 0);
+    assert(heap1.isEmpty() && heap2.isEmpty());
+}
+
 int main() {
+    floats_test();
+    merge_test();
+
     BHeap<int> heap;
 
     heap.insert(20);
@@ -38,8 +65,6 @@ int main() {
     copy_assignment = heap;
 
     assert(heap.extractMin() == 30);
-
-    floats_test();
 
     return 0;
 }
